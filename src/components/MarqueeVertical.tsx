6e4c738ed57a5eb7462526d04b9425880aa97aaf
rault <rtorrelis782@gmail.com>
2024-12-01 00:01:48 +0100

    Commit inicial

---
+++ src/components/MarqueeVertical.tsx
@@ -0,0 +1,92 @@
+import {useState, useEffect} from "react";
+import {supabase} from '../lib/supabase.ts';
+import {cn} from "@/lib/utils";
+import Marquee from "@/components/ui/marquee";
+
+interface User {
+  name: string;
+  username: string;
+  body: string;
+  img: string;
+}
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        "relative overflow-hidden rounded-xl border p-4",
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <div className="overflow-hidden rounded-full w-8 h-8 flex-shrink-0">
+          <img className="w-full h-full object-cover" alt="" src={img} />
+        </div>
+        <div className="flex flex-col w-32 overflow-hidden">
+          <figcaption className="text-sm font-medium dark:text-white truncate">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium dark:text-white/40 truncate">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
+export default function MarqueeHorizontal() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [duration, setDuration] = useState(20);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const {data, error} = await supabase.from("users").select("*");
+      if (error) {
+        console.error("Error fetching users:", error);
+      } else {
+        const formattedUsers = data.map((user: any) => ({
+          name: user.username,
+          username: `@${user.name}`,
+          body: "¡Se ha unido a Magical20!",
+          img: user?.avatar_url ?? `https://avatar.vercel.sh/${user.username}`,
+        }));
+        setUsers(formattedUsers);
+        setDuration(Math.max(400, formattedUsers.length * 2));
+      }
+    }
+
+    fetchUsers();
+  }, []);
+
+  // Dividimos los usuarios en dos filas
+  const firstRow = users.slice(0, users.length / 2);
+  const secondRow = users.slice(users.length / 2);
+
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg z-10 max-h-[600px]">
+      <Marquee style={{ '--duration': `${duration}s` }}>
+        {firstRow.map((user) => (
+          <ReviewCard key={user.username} {...user} />
+        ))}
+      </Marquee>
+      <Marquee reverse style={{ '--duration': `${duration}s` }}>
+        {secondRow.map((user) => (
+          <ReviewCard key={user.username} {...user} />
+        ))}
+      </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-2/6 bg-gradient-to-r from-white via-white/50 to-transparent dark:from-background dark:via-background/50 dark:to-transparent"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-2/6 bg-gradient-to-l from-white via-white/50 to-transparent dark:from-background dark:via-background/50 dark:to-transparent"></div>
+    </div>
+  );
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,119 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ['class'],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			},
+    			'color-1': 'hsl(var(--color-1))',
+    			'color-2': 'hsl(var(--color-2))',
+    			'color-3': 'hsl(var(--color-3))',
+    			'color-4': 'hsl(var(--color-4))',
+    			'color-5': 'hsl(var(--color-5))'
+    		},
+    		animation: {
+    			'shiny-text': 'shiny-text 8s infinite',
+    			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
+    			'fade-up': 'fade-up 1s ease-out forwards',
+    			marquee: 'marquee var(--duration) infinite linear',
+    			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
+    			rainbow: 'rainbow var(--speed, 2s) infinite linear'
+    		},
+    		keyframes: {
+    			'shiny-text': {
+    				'0%, 90%, 100%': {
+    					'background-position': 'calc(-100% - var(--shiny-width)) 0'
+    				},
+    				'30%, 60%': {
+    					'background-position': 'calc(100% + var(--shiny-width)) 0'
+    				}
+    			},
+    			'border-beam': {
+    				'100%': {
+    					'offset-distance': '100%'
+    				}
+    			},
+    			'fade-up': {
+    				'0%': {
+    					opacity: '0',
+    					transform: 'translateY(20px)'
+    				},
+    				'100%': {
+    					opacity: '1',
+    					transform: 'translateY(0)'
+    				}
+    			},
+    			marquee: {
+    				from: {
+    					transform: 'translateX(0)'
+    				},
+    				to: {
+    					transform: 'translateX(calc(-100% - var(--gap)))'
+    				}
+    			},
+    			'marquee-vertical': {
+    				from: {
+    					transform: 'translateY(0)'
+    				},
+    				to: {
+    					transform: 'translateY(calc(-100% - var(--gap)))'
+    				}
+    			},
+    			rainbow: {
+    				'0%': {
+    					'background-position': '0%'
+    				},
+    				'100%': {
+    					'background-position': '200%'
+    				}
+    			}
+    		}
+    	}
+    },
+	plugins: [require("tailwindcss-animate")],
+}
